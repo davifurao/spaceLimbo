e014a76145562e2d57dd65280384d63309047102
Davi Luna <Sdavi738@gmail.com>
2024-02-18 21:46:07 -0300

    fix:adding guildId and automatically deploying the commands in server

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-
+const deployCommands = require('./deploy-command.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 
 client.cooldowns = new Collection();
 
 client.commands = new Collection();
+
+client.on('guildCreate', guild => {
+    // Adicionando o guildId ao config.json
+    addGuildIdToConfig(guild.id);
+});
+
+client.on('guildCreate', guild => {
+    console.log(`Bot was added to ${guild.name} (${guild.id})`);
+    // Deploy commands when bot is added to a guild
+    deployCommands(guild.id);
+});
+
+function addGuildIdToConfig(guildId) {
+    try {
+        const config = require('./config.json');
+        config.guildId = guildId;
+        fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
+        console.log('GuildId adicionado ao config.json:', guildId);
+    } catch (error) {
+        console.error('Erro ao adicionar guildId ao config.json:', error);
+    }
+}
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
